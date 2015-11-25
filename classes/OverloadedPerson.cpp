2cd5829a0e0d8789e9b90295abb5023af37deb6b
Damian Wojakowski <wojakowski.damian@gmail.com>
2015-11-25 08:25:21 +0100

    classes - test overloading operators

---
+++ classes/OverloadedPerson.cpp
@@ -0,0 +1,18 @@
+/*
+ * OverloadedPerson.cpp
+ *
+ *  Created on: 25 lis 2015
+ *      Author: damian
+ */
+
+#include "OverloadedPerson.h"
+
+OverloadedPerson::OverloadedPerson() {
+    // TODO Auto-generated constructor stub
+
+}
+
+OverloadedPerson::~OverloadedPerson() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ classes/OverloadedPerson.h
@@ -0,0 +1,17 @@
+/*
+ * OverloadedPerson.h
+ *
+ *  Created on: 25 lis 2015
+ *      Author: damian
+ */
+
+#ifndef SRC_CLASSES_OVERLOADEDPERSON_H_
+#define SRC_CLASSES_OVERLOADEDPERSON_H_
+
+class OverloadedPerson {
+public:
+    OverloadedPerson();
+    virtual ~OverloadedPerson();
+};
+
+#endif /* SRC_CLASSES_OVERLOADEDPERSON_H_ */
